13b7ed9b2cd4ac3c21530ae2edde429502caad21
Sil silas <silaswans@gmail.com>
2020-09-10 23:50:04 +0300

    first commit

---
+++ app/src/main/java/com/sil/books/Book.java
@@ -0,0 +1,10 @@
+package com.sil.books;
+
+public class Book {
+    public String id;
+    public String title;
+    public String subTitle;
+    public String[] authors;
+    public String publisher;
+    public String publishedDate;
+}
