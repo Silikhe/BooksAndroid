6b4dbf14d2cd2aaf3dfacf705f6f54af2d50d35a
Sil silas <silaswans@gmail.com>
2020-09-14 21:06:35 +0300

    first commit

--- app/src/main/java/com/sil/books/Book.java
+++ app/src/main/java/com/sil/books/Book.java
@@ -18,4 +18,12 @@ public class Book {
         this.publisher = publisher;
         this.publishedDate = publishedDate;
     }
+    public Book(String id, String title, String subTitle, String[] authors, String publisher, String publishedDate) {
+        this.id = id;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.publishedDate = publishedDate;
+    }
 }
